dd876921833dae2392c23aa28e8efc3b0e01ba2d
David Ford <dford@smart-soft.com>
2017-06-30 16:13:49 -0700

    Add suitName getter to Card

--- src/main/kotlin/bj1/Card.kt
+++ src/main/kotlin/bj1/Card.kt
@@ -1,3 +1,19 @@
 package bj1
 
-class Card(val value:Int,val suit:Int)
+class Card(val value: Int, val suit: Int) {
+
+    val suitName: String get() = when (suit) {
+        1 -> "Spades"
+        2 -> "Hearts"
+        3 -> "Clubs"
+        4 -> "Diamonds"
+        else -> throw IllegalStateException()
+    }
+
+    val valueName: String get() {
+        return "Ace"
+    }
+
+
+
+}
--- src/test/kotlin/bj1/CardTest.kt
+++ src/test/kotlin/bj1/CardTest.kt
@@ -3,19 +3,38 @@ package bj1
 import org.junit.Test
 import kotlin.test.assertEquals
 
-class CardTest{
+class CardTest {
 
     @Test
     fun t1() {
 
-        val c1 = Card(1,1)
-        val c2 = Card(13,4)
+        val c1 = Card(value = 1, suit = 1)
+        val c2 = Card(value = 13, suit = 4)
 
-        assertEquals(1,c1.value)
-        assertEquals(1,c1.suit)
+        assertEquals(1, c1.value)
+        assertEquals(1, c1.suit)
+        assertEquals("Spades", c1.suitName)
 
-        assertEquals(13,c2.value)
-        assertEquals(4,c2.suit)
+        assertEquals(13, c2.value)
+        assertEquals(4, c2.suit)
+        assertEquals("Diamonds", c2.suitName)
+
+    }
+
+    @Test
+    fun t2() {
+
+        val x = if (3 < 4) "aa" else "bb"
+
+        val suit = 2
+        val y = when (suit) {
+            1 -> "Spades"
+            2 -> "Hearts"
+            else -> "Oops"
+        }
+
+        println("x = ${x}")
+        println("y = ${y}")
 
     }
 
